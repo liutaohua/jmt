d1382e09ed6626805bb393686145ee93499f0b4b
PierreWang <dragon5151@163.com>
2017-09-26 22:01:14 +0800

    add some method for Connections

--- src/main/java/cc/tpark/connections/Connections.java
+++ src/main/java/cc/tpark/connections/Connections.java
@@ -1,6 +1,7 @@
 package cc.tpark.connections;
 
 import cc.tpark.commons.InnerMsg;
+import io.netty.channel.ChannelHandlerContext;
 
 public interface Connections {
     /**
@@ -9,5 +10,21 @@ public interface Connections {
      * @param ip
      * @param msg
      */
-    public void sendMsg(String ip, InnerMsg msg);
+    void sendMsg(String ip, InnerMsg msg);
+
+    /**
+     * 新增连接
+     *
+     * @param ip
+     * @param ctx
+     */
+    boolean addConnect(String ip, ChannelHandlerContext ctx);
+
+    /**
+     * 删除连接
+     *
+     * @param ip
+     */
+    void removeConnect(String ip);
+
 }
